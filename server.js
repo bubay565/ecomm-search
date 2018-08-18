e6a2e3be63d814d63331eb9a31b648b6b5b33d49
Buwa <bubay565@yahoo.com>
2018-08-18 22:53:08 +0100

    refactor to fetch payload

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
-const request = require('request')
+const port = process.env.PORT || 8081;
 
-app.get('/api/data', (req,res) => {
-        res.send(require('./payload.JSON'))
-})
-
-app.listen(8081, () => console.log('backend service running🤓'))
+app.get('/api/data', (req, res) => {
+  const fs = require('fs');
+  let dataObj;
+  fs.readFile('./payload.JSON', 'utf8', (err, data) => {
+    if (err) {
+       res.send(JSON.parse(err));
+    }
+    dataObj = JSON.parse(data);
+    res.send(dataObj)
+  });
+});
+  
+app.listen(port, () => console.log('backend service running🤓')) 
 
