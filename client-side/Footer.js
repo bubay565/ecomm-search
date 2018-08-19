10cdf7a794fa28e64a0c041244a27c2953161323
Buwa <bubay565@yahoo.com>
2018-08-19 15:58:57 +0100

    footer and some css mobile view to be implemented

---
+++ client-side/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Designed and developed for Argos Digital UK &copy; 2018</p>
+        </div>
+    )
+}
+
+export default Footer
